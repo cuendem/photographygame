b6f0a3e55061c57e2b4e035da5905a24e8e0a585
Ana <anafedi8.0@gmail.com>
2025-05-12 20:46:54 +0200

    Add MiddleTrigger and OutsideTrigger classes for collision detection

---
+++ Assets/Scripts/MiddleTrigger.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleTrigger:MonoBehaviour
+{
+    private HashSet<Collider2D> overlappingObjects = new HashSet<Collider2D>();
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        overlappingObjects.Add(other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        overlappingObjects.Remove(other);
+    }
+
+    public HashSet<Collider2D> GetMiddle()
+    {
+        return overlappingObjects;
+    }
+}
---
+++ Assets/Scripts/OutsideTrigger.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutsideTrigger:MonoBehaviour
+{
+    private HashSet<Collider2D> overlappingObjects = new HashSet<Collider2D>();
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        overlappingObjects.Add(other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        overlappingObjects.Remove(other);
+    }
+
+    public HashSet<Collider2D> GetOutside()
+    {
+        return overlappingObjects;
+    }
+}
